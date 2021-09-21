82d16c8e0308817f25b2fb570aed2c3ee0be1317
Starboy710 <40715430+Starboy710@users.noreply.github.com>
2021-09-21 17:09:57 +0530

    changed to java code

---
+++ AddTwoNumbers.java
@@ -0,0 +1,12 @@
+public class AddTwoNumbers {
+
+	public static void main(String[] args) {
+		
+		int a, b;
+		a = 3;
+		b = 5;
+		System.out.println("The addition of numbers: " + (a+b));
+
+	}
+
+}
